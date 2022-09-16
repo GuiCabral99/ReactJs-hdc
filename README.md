1f3cf191d466b5155db2324f2c3ddee4c6a8dd6a
Guilherme Cabral <gui.cabral7641@gmail.com>
2022-09-16 06:14:46 -0300

    Iniciando em react hooks

--- README.md
+++ README.md
@@ -14,3 +14,10 @@
     NomeComponente.defaultProps = {
     NomeVar1: "mensagem padrão",
     NomeVar2: "mensagem padrão 2" };
+
+# React Hook
+- Importando o useState:
+    import { React, useState } from "react";
+
+- Criando uma variavel:
+    const [exemplo, setExemplo] = useState();
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,32 @@
+import { React, useState } from "react";
+
 function Formulario() {
 
     function cadastrarUsuario(e) {
-        e.preventDefault()
-        console.log("usuario cadastrado")
+        e.preventDefault();
+        console.log("usuario cadastrado");
+        console.log(nome)
     }
+
+    const [nome, setNome] = useState();
 
     return ( 
         <>
             <h2>Formulário</h2>
-            <form onSubmit={cadastrarUsuario}>
+            <form>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
-                    <input type="submit" value="Cadastrar" />
+                    <label htmlFor="nome">Nome: </label>
+                    <input
+                    type="text"
+                    name="nome"
+                    id="nome" 
+                    placeholder="Digite seu nome"
+                    onChange={(e) => {setNome(e.target.value); console.log(nome)}} />
+
+                    <label htmlFor="senha">Senha: </label>
+                    <input type="text" name="senha" id="senha" placeholder="Digite uma senha" />
+
+                    <input type="submit" value="Cadastrar" onClick={cadastrarUsuario} />
                 </div>
             </form>
         </>
